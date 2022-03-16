1dcd22f1c6db68f58fe93bcc95d8302da853378f
EC2 Default User <ec2-user@ip-172-31-95-22.ec2.internal>
2022-03-16 08:07:19 +0000

    [Add]更新機能追加中

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -25,6 +25,11 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  
   private
   # ストロングパラメータ
   def list_params
